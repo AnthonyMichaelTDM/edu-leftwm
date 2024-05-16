82aa48038869bb22b7dfcff003198d062756c6d2
AnthonyMichaelTDM <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2024-05-16 02:06:32 -0700

    chore: Update wal.sh script to handle template directory and file creation

--- leftwm/themes/genesis/scripts/wal.sh
+++ leftwm/themes/genesis/scripts/wal.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 ### Check if python-pywal is installed
 if [ -x "$(command -v wal)" ]; then
+  ### Check if template dir exists, then put template in its proper place
+  if [ ! -d $HOME/.config/wal/templates ] ; then
+    mkdir $HOME/.config/wal/templates &&
+    cp $HOME/.config/leftwm/themes/current/template-wal/color.leftwm-theme.ron  $HOME/.config/wal/templates
+  elif [ ! -f $HOME/.config/wal/templates/color.leftwm-theme.ron ] ; then
+    cp $HOME/.config/leftwm/themes/current/template-wal/color.leftwm-theme.ron  $HOME/.config/wal/templates
+  fi
+
   #use pywal to generate color schemes for almost everything
   wal -i /usr/share/backgrounds/ -a 80
 else
